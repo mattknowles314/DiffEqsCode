fa2094786d4f7eca4e830ec95d04e9ddfa58dbcc
Matthew Knowles <mattknowles314@gmail.com>
2020-11-27 10:18:46 +0000

    started working

--- adBu.py
+++ adBu.py
@@ -1,3 +1,24 @@
 '''
-Two-Step Adam's Bushforth Method
+Two-Step Adam's Bashforth Method
+
+- https://ogden.eu/the-two-step-adams-bashforth-method-with-different-stepsizes -
+
+y_(n+1) = y_n + 3/2*h*f(t_n,y_n) - 1/2*h*f(t_(n-1),t_(n-1))
+
 '''
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+'''
+    ode_solve: Performs Adams' Bashforth method on a given function f
+
+    f (callable): first order ODE to be solved
+    y0 (array): initial condition
+    t (array): a sequence of time points to solve the ODE at
+    args (dict): extra arguments that get passed
+'''
+def ode_solve(f, y0, t, args={}):
+    y = np.zeros([len(t), len(y0)])
+    y[0] = y0
+
